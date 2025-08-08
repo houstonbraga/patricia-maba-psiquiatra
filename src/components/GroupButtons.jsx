ac04f2e45deff7df5478f65533bcd20a67562bc6
Houston Braga <houstonliverboy123@gmail.com>
2025-08-08 11:13:09 -0300

    feat: add links social media

--- src/components/GroupButtons.jsx
+++ src/components/GroupButtons.jsx
@@ -5,7 +5,7 @@ const GroupButtons = ({ onShowTestimonials, onShowTrajectory }) => {
   return (
     <div className="mt-5 w-full flex flex-col items-center gap-3 px-6">
       <Button onClick={onShowTrajectory}>Minha Trajetória</Button>
-      <ButtonLink url="#">Agende sua consulta aqui</ButtonLink>
+      <ButtonLink url="https://api.whatsapp.com/send/?phone=%2B5548996510605&text=Ol%C3%A1+vim+do+site+da+Dra.+Patricia+Maba+e+gostaria+de+agendar+uma+consulta+on-line+particular.&type=phone_number&app_absent=0">Agende sua consulta aqui</ButtonLink>
       <Button onClick={onShowTestimonials}>Depoimentos de pacientes</Button>
       <ButtonLink url="https://drapatriciamaba.com.br/">
         Acessar meu site <Globe width={20} />
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -4,7 +4,11 @@ import whatsapp from "../assets/whatsapp.svg";
 
 export function Instagram() {
   return (
-    <a href="#" className="hover:scale-110 transition-transform">
+    <a
+      href="https://www.instagram.com/dra.patriciamaba/"
+      target="_blank"
+      className="hover:scale-110 transition-transform"
+    >
       <img src={instagram} alt="instagram-link" />
     </a>
   );
@@ -12,7 +16,11 @@ export function Instagram() {
 
 export function Email() {
   return (
-    <a href="#" className="hover:scale-110 transition-transform">
+    <a
+      href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#search/drapatriciamaba%40gmail.com"
+      target="_blank"
+      className="hover:scale-110 transition-transform"
+    >
       <img src={email} alt="email-link" />
     </a>
   );
@@ -20,7 +28,11 @@ export function Email() {
 
 export function WhatsApp() {
   return (
-    <a href="#" className="hover:scale-110 transition-transform mt-1">
+    <a
+      href="https://api.whatsapp.com/send/?phone=%2B5548996510605&text=Ol%C3%A1+vim+do+site+da+Dra.+Patricia+Maba+e+gostaria+de+agendar+uma+consulta+on-line+particular.&type=phone_number&app_absent=0"
+      target="_blank"
+      className="hover:scale-110 transition-transform mt-1"
+    >
       <img src={whatsapp} alt="whatsapp-link" />
     </a>
   );
