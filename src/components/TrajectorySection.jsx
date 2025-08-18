dc1d514ed7b04dc1d1ac0ae88743f9585b21bacd
Houston Braga <houstonliverboy123@gmail.com>
2025-08-18 12:58:30 -0300

    refactor: edit Trajetory text

--- src/components/TrajectorySection.jsx
+++ src/components/TrajectorySection.jsx
@@ -39,23 +39,24 @@ const TrajectorySection = ({ isVisible, onClose }) => {
           <div className="space-y-4 text-custom-green font-poppins">
             <p className="text-sm leading-relaxed">
               Olá! Sou a Dra. Patrícia Maba e estou aqui para cuidar da sua
-              saúde mental, pensando em você, mulher, com toda a sua
-              complexidade e desafios. Sei que buscar ajuda pode ser um passo
-              grande, e meu objetivo é que você se sinta completamente acolhida
-              e compreendida nesse processo.
+              saúde mental, considerando toda a sua complexidade e desafios. Sei
+              que buscar ajuda pode ser um passo importante, e meu objetivo é
+              que você se sinta plenamente acolhido(a) e compreendido(a) nesse
+              processo. Essa trajetória me permite ter um olhar amplo, humano e
+              atualizado para oferecer o melhor cuidado a você.
             </p>
 
             <p className="text-sm leading-relaxed">
-              Minha jornada profissional começou com a Farmácia, o que me deu
-              uma base sólida em como o corpo e a mente se interligam. Depois,
-              me formei em Medicina pela Universidade do Sul de Santa Catarina
-              (Unisul) e, hoje, estou me especializando ainda mais em
-              Psiquiatria pela Santa Casa de São Paulo.
+              Minha jornada profissional começou na Farmácia, o que me deu uma
+              base sólida sobre como corpo e mente se interligam. Formei-me em
+              Medicina pela Universidade do Sul de Santa Catarina e, atualmente,
+              estou me especializando em Psiquiatria pela Faculdade de Ciências
+              Médicas da Santa Casa de São Paulo.
             </p>
 
             <p className="text-sm leading-relaxed">
-              Toda essa formação me permite ter um olhar amplo e atualizado para
-              oferecer o melhor cuidado a você.
+              Essa trajetória me permite ter um olhar amplo, humano e atualizado
+              para oferecer o melhor cuidado a você.
             </p>
           </div>
         </div>
